a0c628e19188a35c1939f86867e66b7136031ea4
Thawon Uttamavanit <uttamavanit@gmail.com>
2015-04-23 22:58:37 +1000

    protractor test

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,29 +2,36 @@
     "use strict";
 
     angular.module("app", [
-        "ngNewRouter",
+        "ui.router",
         "app.home",
         "app.page"
     ])
-    .config(["$locationProvider", "$componentLoaderProvider", function ($locationProvider, $componentLoaderProvider) {
+    .config(["$locationProvider", "$stateProvider", "$urlRouterProvider",
+    function ($locationProvider, $stateProvider, $urlRouterProvider) {
         $locationProvider.html5Mode({
             enabled: true
         });
 
-        $componentLoaderProvider.setTemplateMapping(function (name) {            
-            var dashName = dashCase(name);            
-            return "./scripts/components/" + dashName + "/" + dashName + ".html";
-        });
-    } ])
+        $urlRouterProvider.otherwise("/home");
+
+        $stateProvider
+            .state('home', {
+                url: "/home",
+                templateUrl: "scripts/components/home/home.html",
+                controller: "HomeController as home"
+            })
+            $stateProvider
+            .state('page', {
+                url: "/page",
+                templateUrl: "scripts/components/page/page.html",
+                controller: "PageController"
+            })
+
+    }])
     .controller("AppController", AppController);
 
-    AppController.$inject = ["$router"];
 
-    function AppController($router) {
-        $router.config([
-			{ path: "/", component: "home" },
-            { path: "/page", component: "page" }
-        ]);
+    function AppController() {
     }
 
 } ());
--- test/e2e/auth-spec.js
+++ test/e2e/auth-spec.js
@@ -8,6 +8,9 @@ describe("authentication", function () {
             .run(function ($httpBackend) {
                 $httpBackend.whenPOST(/test/).respond(200, { field: 'Yeah YEah Yeah' });
 
+                $httpBackend.whenGET("scripts/components/home/home.html").passThrough();
+
+                $httpBackend.expectGET("scripts/components/home/home.html")
                 //$httpBackend.expectGET(/test/);
             });
     };
